6e8cd15be8ce89a9bc759b58e7f99f78ade1c4d1
pernwn <pernillepjohansen@hotmail.com>
2024-06-03 20:18:33 +0200

    metadata

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,8 @@
-'use client'
 
 import '@/app/globals.css'
 import { Josefin_Sans, Lato } from 'next/font/google';
 import Head from 'next/head';
-import { metadata } from './metadata';
+
 
 const josefin_Sans = Josefin_Sans({
   weight: "variable",
@@ -17,38 +16,20 @@ const lato = Lato({
   variable:"--font-lato"
 });
 
+export const metadata = {
+  title: {
+    template: "%s | CyberMinds",
+    default: "CyberMinds",
+  },
+  description: "Victoria W Johansen - Multimediedesign, Hovedopgave 2024",
+};
+
 
 export default function RootLayout({ children }) {
   
 
   return (
     <html lang="en" className="background--custom" >
-      <Head>
-        <title>{metadata.title.default}</title>
-        <meta
-          name="description"
-          content={metadata.description}
-        />
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
       <body
         className={`${josefin_Sans.variable} ${lato.variable} mx-auto max-w-stretch overflow-x-hidden`}
       >
--- app/metadata.js
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  title: {
-    template: "%s | CyberMinds",
-    default: "CyberMinds",
-  },
-  description: "Victoria W Johansen - Multimediedesign, Hovedopgave 2024",
-};
